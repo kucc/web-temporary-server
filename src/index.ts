fa8ea72d107d9a5573613871e3d45c78af2b4e7c
riverandeye <riverandeye@gmail.com>
2020-01-21 21:38:39 +0900

    <feat>: bind user router to app, add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 dotenv.config();
 
+import UserRouter from './route/user-route';
+
 const app = express();
+
+app.use(express.json());
+app.use('/users', UserRouter);
+app.use(cors());
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('hello');
