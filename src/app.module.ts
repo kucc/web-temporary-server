0e83daa3a09ab0a98d30dc0aa6536657869d4969
riverandeye <riverandeye@gmail.com>
2020-01-31 23:11:26 +0900

    <feat>: apply middleware for deserializing user information

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { DeserializeUserMiddleWare } from './common/middleware/deserialize-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DeserializeUserMiddleWare).forRoutes('*');
+  }
+}
