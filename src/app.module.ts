8baca05904e62b5329550744b2944cbf92663ca4
easymean <m2162003@gmail.com>
2020-01-31 22:37:13 +0900

    <feat>: add user-project module to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectModule } from './project/project.module';
+import { UserProjectModule } from './user-project/user-project.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, ProjectModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, ProjectModule, UserProjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
