f288eac492d72851e6d6be865a64d54af1579570
jiseongg <jiseongg301@gmail.com>
2020-02-04 02:49:10 +0900

    <feat>: add api for updating likes for the comment

---
+++ src/comment-like/comment-like.service.ts
@@ -0,0 +1,33 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CommentLikeEntity } from './comment-like.entity';
+
+@Injectable()
+export class CommentLikeService {
+  public constructor(
+    @InjectRepository(CommentLikeEntity)
+    private readonly commentLikeRepository: Repository<CommentLikeEntity>,
+  ) {}
+
+  public async toggleLikes(
+    commentId: number,
+    userId: number,
+  ): Promise<boolean> {
+    const CommentLike = await this.commentLikeRepository.findOne({
+      where: {
+        commentId,
+        userId,
+      },
+    });
+
+    if (!CommentLike) {
+      await this.commentLikeRepository.insert({ commentId, userId });
+      return true;
+    }
+
+    await this.commentLikeRepository.delete({ commentId, userId });
+    return false;
+  }
+}
