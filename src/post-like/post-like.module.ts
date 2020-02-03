ac901f125cfab981df5109ba93f7a79214c944c6
jiseongg <jiseongg301@gmail.com>
2020-02-03 18:21:53 +0900

    <feat>: add api for update likes of posts

--- src/post-like/post-like.module.ts
+++ src/post-like/post-like.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PostLikeService } from './post-like.service';
+import { PostLikeEntity } from './post-like.entity';
 
 @Module({
-  providers: [PostLikeService]
+  imports: [TypeOrmModule.forFeature([PostLikeEntity])],
+  providers: [PostLikeService],
+  exports: [PostLikeService],
 })
 export class PostLikeModule {}
