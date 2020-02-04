b73f470b673767c55e108472089a0274c2c4a33f
jiseongg <jiseongg301@gmail.com>
2020-02-04 15:29:19 +0900

    <feat>: add api for creating reply for comments

--- src/comment-reply/comment-reply.service.ts
+++ src/comment-reply/comment-reply.service.ts
@@ -1,4 +1,19 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CommentReplyEntity } from './comment-reply.entity';
 
 @Injectable()
-export class CommentReplyService {}
+export class CommentReplyService {
+  public constructor(
+    @InjectRepository(CommentReplyEntity)
+    public readonly commentReplyRepository: Repository<CommentReplyEntity>,
+  ) {}
+
+  public async addParentChildRelation(parentId: number, childId: number) {
+    await this.commentReplyRepository.insert({ parentId, childId });
+
+    return { return: true };
+  }
+}
