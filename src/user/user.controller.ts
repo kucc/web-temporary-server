08d63560cf351a9f22d91ef912b0f744568aef1b
riverandeye <riverandeye@gmail.com>
2020-02-07 22:45:21 +0900

    <feat>: add update user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,7 @@ import {
   Param,
   Body,
   NotFoundException,
+  Put,
 } from '@nestjs/common';
 
 import { UserService } from './user.service';
@@ -17,7 +18,7 @@ import { VerifyEmailRequestDTO } from './dto/verify-email-request.dto';
 export class UserController {
   public constructor(private readonly userService: UserService) {}
 
-  @Post('')
+  @Post('/signup')
   async signUp(
     @Body() userRequestDTO: UserRequestDTO,
   ): Promise<UserResponseDTO> {
@@ -27,11 +28,11 @@ export class UserController {
 
   @Post('/email-verification')
   async verifyEmail(@Body() verifyEmailRequestDTO: VerifyEmailRequestDTO) {
-    const User = await this.userService.findUserByEmail(
+    const user = await this.userService.findUserByEmail(
       verifyEmailRequestDTO.email,
     );
 
-    if (!User) {
+    if (!user) {
       return { result: true };
     }
 
@@ -42,14 +43,32 @@ export class UserController {
   async getUserById(
     @Param('Id', ValidateIdPipe) Id: number,
   ): Promise<UserResponseDTO> {
-    const User = await this.userService.findUserById(Id);
+    const user = await this.userService.findUserById(Id);
 
-    if (!User) {
+    if (!user) {
       throw new NotFoundException(
         `${Id}번 Id를 갖는 회원이 존재하지 않습니다.`,
       );
     }
 
-    return new UserResponseDTO(User);
+    return new UserResponseDTO(user);
+  }
+
+  @Put('/:Id')
+  async updateUser(
+    @Param('Id', ValidateIdPipe) Id: number,
+    @Body() userRequestDTO: UserRequestDTO,
+  ): Promise<UserResponseDTO> {
+    const user = await this.userService.findUserById(Id);
+
+    if (!user) {
+      throw new NotFoundException(
+        `${Id}번 Id를 갖는 회원이 존재하지 않습니다.`,
+      );
+    }
+
+    const updatedUser = await this.userService.updateUser(user, userRequestDTO);
+
+    return new UserResponseDTO(updatedUser);
   }
 }
