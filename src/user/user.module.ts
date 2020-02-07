2fc730fe31d89b02dc65646356a592d1fa48985e
riverandeye <riverandeye@gmail.com>
2020-02-08 02:36:55 +0900

    <feat>: imprort postmodule in usermodule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
+import { PostModule } from '../post/post.module';
+import { UserController } from './user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), PostModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
