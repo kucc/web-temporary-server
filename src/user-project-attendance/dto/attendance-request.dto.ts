1d0c063909088f3b908b863929a2b5b005a8afc4
easymean <m2162003@gmail.com>
2020-02-11 19:47:40 +0900

    <fix>: modify attendance API

--- src/user-project-attendance/dto/attendance-request.dto.ts
+++ src/user-project-attendance/dto/attendance-request.dto.ts
@@ -3,7 +3,6 @@ import { IsNumber, IsEmpty } from 'class-validator';
 export class AttendanceRequestDTO {
   @IsEmpty()
   public userProjectId: number;
-
   public readonly attendanceTypeId: number;
   public readonly description: string;
 }
