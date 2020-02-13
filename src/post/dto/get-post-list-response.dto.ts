fa83cea1b5f68e43628809111af57bb82b165bb1
jiseongg <jiseongg301@gmail.com>
2020-02-13 16:06:50 +0900

    <feat>: modify DTOs for PostEntity

--- src/post/dto/get-post-list-response.dto.ts
+++ src/post/dto/get-post-list-response.dto.ts
@@ -1,19 +1,13 @@
 import { PostEntity } from '../post.entity';
+import { GetPostListElementDTO } from './get-post-list-element.dto';
 
 export class GetPostListResponseDTO {
-  public constructor(posts: PostEntity[]) {
+  public constructor(posts: PostEntity[], totalCount: number) {
+    this.totalCount = totalCount;
     this.count = posts.length;
-    this.data = posts.map(post => {
-      return {
-        title: post.title,
-        userId: post.userId,
-        postTypeId: post.postTypeId,
-        createdAt: post.createdAt,
-        likes: post.likes,
-        views: post.views,
-      };
-    });
+    this.data = posts.map(post => new GetPostListElementDTO(post));
   }
+  public readonly totalCount: number;
   public readonly count: number;
   public readonly data: object;
 }
