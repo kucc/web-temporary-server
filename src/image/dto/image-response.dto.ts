d137ecdddcc382b45881119819a984afbab40ff0
easymean <m2162003@gmail.com>
2020-02-19 00:31:53 +0900

    <refactor>: change column name

--- src/image/dto/image-response.dto.ts
+++ src/image/dto/image-response.dto.ts
@@ -6,12 +6,12 @@ export class ImageResponseDTO {
     this.name = image.name;
     this.postId = image.postId; //보류
     this.url = image.url;
-    this.IsRepresentative = image.IsRepresentative;
+    this.isRepresentative = image.isRepresentative;
   }
 
   public readonly Id: number;
   public readonly name: string;
   public readonly url: string;
   public readonly postId: number;
-  public readonly IsRepresentative: boolean;
+  public readonly isRepresentative: boolean;
 }
