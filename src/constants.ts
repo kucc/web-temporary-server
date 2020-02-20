d19e145e9bccdd6f43d1bddc85c57769e8c10deb
easymean <m2162003@gmail.com>
2020-02-20 13:40:39 +0900

    <fix>: set POST_TYPE as string

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const POSTS_PER_PAGE = 10;
-export const IMAGES_PER_PAGE = 15;
 
 export enum ATTENDANCE_TYPE {
   LATE = 'late',
@@ -9,8 +8,8 @@ export enum ATTENDANCE_TYPE {
 }
 
 export enum POST_TYPE {
-  NOTICE = 1,
-  FREE = 2,
-  GALLERY = 3,
-  ALUMNI = 4,
+  NOTICE = 'NOTICE',
+  FREE = 'FREE',
+  GALLERY = 'GALLERY',
+  ALUMNI = 'ALUMNI',
 }
