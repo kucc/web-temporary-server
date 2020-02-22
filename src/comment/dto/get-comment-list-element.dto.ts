0509ef618fe905d0d34ceceace7817e1d27d346b
jiseongg <jiseongg301@gmail.com>
2020-02-22 19:26:52 +0900

    <feat>: modify DTOs

--- src/comment/dto/get-comment-list-element.dto.ts
+++ src/comment/dto/get-comment-list-element.dto.ts
@@ -2,12 +2,16 @@ import { CommentEntity } from '../comment.entity';
 
 export class GetCommentListElementDTO {
   public constructor(comment: CommentEntity) {
+    this.Id = comment.Id;
     this.content = comment.content;
+    this.postId = comment.postId;
     this.userId = comment.userId;
     this.createdAt = comment.createdAt;
     this.likes = comment.likes;
   }
+  public readonly Id: number;
   public readonly content: string;
+  public readonly postId: number;
   public readonly userId: number;
   public readonly createdAt: string;
   public readonly likes: number;
