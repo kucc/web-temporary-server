5640121892b91d76e003dabf1309ed6b989a6799
easymean <m2162003@gmail.com>
2020-02-22 16:42:39 +0900

    <chore>: remove console.log

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -25,7 +25,6 @@ export class ImageService {
     createImagesBodyDTO: CreateImageBodyDTO[],
     postId: number,
   ): Promise<ImageEntity[]> {
-    console.log(createImagesBodyDTO);
     const images = this.imageRepository
       .create(createImagesBodyDTO)
       .map(imageEntity => {
