d50412356e3d03a34a146acb8e36f9ddfe5e64e5
riverandeye <riverandeye@gmail.com>
2020-03-30 23:47:34 +0900

    <fea>: set prefix - api

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ const corsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.use(cors(corsOptions));
